06e01f8bc5b497d57220b50a0ad96ec00c30074a
doxas <eizanco@gmail.com>
2017-09-30 21:16:26 +0900

    change normalize mode

--- index.js
+++ index.js
@@ -5,7 +5,32 @@ let fs = require('fs');
 
 let dest = {};
 let paths = {
-    hajicco: './resource/hajicco.svg'
+    S1:  './resource/msg/msgS_001.svg',
+    S2:  './resource/msg/msgS_002.svg',
+    S3:  './resource/msg/msgS_003.svg',
+    S4:  './resource/msg/msgS_004.svg',
+    S5:  './resource/msg/msgS_005.svg',
+    S6:  './resource/msg/msgS_006.svg',
+    S7:  './resource/msg/msgS_007.svg',
+    S8:  './resource/msg/msgS_008.svg',
+    S9:  './resource/msg/msgS_009.svg',
+    S10: './resource/msg/msgS_010.svg',
+    S11: './resource/msg/msgS_011.svg',
+    S12: './resource/msg/msgS_012.svg',
+    S13: './resource/msg/msgS_013.svg',
+    M1:  './resource/msg/msgM_001.svg',
+    M2:  './resource/msg/msgM_002.svg',
+    M3:  './resource/msg/msgM_003.svg',
+    M4:  './resource/msg/msgM_004.svg',
+    M5:  './resource/msg/msgM_005.svg',
+    M6:  './resource/msg/msgM_006.svg',
+    M7:  './resource/msg/msgM_007.svg',
+    M8:  './resource/msg/msgM_008.svg',
+    L1:  './resource/msg/msgL_001.svg',
+    L2:  './resource/msg/msgL_002.svg',
+    L3:  './resource/msg/msgL_003.svg',
+    L4:  './resource/msg/msgL_004.svg',
+    L5:  './resource/msg/msgL_005.svg'
 };
 
 for(let i in paths){
@@ -21,7 +46,7 @@ for(let i in paths){
         }
         if(f === true){
             let json = JSON.stringify(dest);
-            fs.writeFile('./dest/out.json', json , (_err) => {
+            fs.writeFile('./dest/msg/out.json', json, (_err) => {
                 if(_err != null){
                     console.log('write error');
                 }
@@ -35,7 +60,8 @@ function readFile(key, path, callback){
         if(err == null){
             let svgPath = parsePath(data);
             let mesh = svgMesh3d(svgPath, {
-                delaunay: true
+                delaunay: true,
+                normalize: false
             });
             callback(err, key, mesh);
         }else{
